08a9f42e83c2715f8d7c486ed97bfd3e79bfd427
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-01-31 16:28:56 +0530

    Font added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 'use strict';
+const btnTranslate = document.querySelector('#btn-translate');
+const btnTxt = document.querySelector('#btn-txt');
+
+
+const clickHandler = function () {
+    console.log(btnTxt.value)
+}
+btnTranslate.addEventListener('click', clickHandler);
