2f47e83ff4b78989828ccf764403156c25948e40
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-02-01 12:53:10 +0530

    input process output done

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 'use strict';
 const btnTranslate = document.querySelector('#btn-translate');
 const btnTxt = document.querySelector('#btn-txt');
+const outBox = document.querySelector('.outputBox');
 
+const urlL = `https://api.funtranslations.com/translate/minion.json`;
+
+function addTextUrl(text) {
+    return urlL + '?' + 'text=' + text;
+
+}
+
+function catchError(error) {
+    console.log("Error Occured ! ", error);
+    alert(`Something wrong with server ! try again after sometime.`)
+}
 
 const clickHandler = function () {
-    console.log(btnTxt.value)
+    const inputTxt = btnTxt.value;
+    fetch(addTextUrl(inputTxt))
+        .then(function (request) {
+            return request.json()
+        })
+        .then(function (data) {
+            outBox.innerHTML = data.contents.translated;
+        })
+        .catch(catchError)
 }
 btnTranslate.addEventListener('click', clickHandler);
